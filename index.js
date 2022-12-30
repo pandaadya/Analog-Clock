8362a9c664e035f34a4ae2490eb1856c0e0c00af
pandaadya <117782518+pandaadya@users.noreply.github.com>
2022-12-30 15:28:26 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+var second=0;
+var minute=0;
+var hour=0;
+var d=new Date();
+
+setInterval(
+    function() {
+        d=new Date();
+        second=d.getSeconds()*6;
+        minute=d.getMinutes()*6;
+        hour=d.getHours()*30+Math.round(minute/12);
+        document.getElementById("second").style.transform="rotate(" + second + "deg)";
+        document.getElementById("hour").style.transform="rotate(" + hour + "deg)";
+        document.getElementById("minute").style.transform="rotate(" + minute + "deg)";
+
+
+    },1000
+);
